bfa39a05ceac76ef35a2a28671aed39b6703a35d
Rajagopal37 <trajagopal37@gmail.com>
2024-05-13 11:14:39 +0530

    function

--- script.js
+++ script.js
@@ -2,4 +2,24 @@
 
  function display(num){
     displayScreen.value += num;
- } 
+ }  
+
+ function calculate( ) {
+   try{
+      displayScreen.value = eval(displayScreen.value)
+   }
+   catch(err)
+   {
+      alert("Invalit Input")
+   }
+ }
+
+ function Clear (){
+   displayScreen.value = "";
+
+ }
+
+ function del (){
+   displayScreen.value = displayScreen.value.slice(0,-1)
+
+ }
